b17a42f1d0da25281f208e2669bf561d5d8ba628
diegomoreira <diegomoreira2001@gmail.com>
2020-11-28 16:37:21 -0300

    Primeiro exercício da apostila

---
+++ Projeto 01 Inicio/ExercícioApostila01.java
@@ -0,0 +1,11 @@
+class ExercicioApostila01 {
+    public static void main(String[] args) {
+        // imprimindo todos os números entre 150 e 300
+        int NumeroInicial = 150;
+        int NumeroFinal = 300;
+            while(NumeroInicial <= NumeroFinal){
+                System.out.println(NumeroInicial);
+                NumeroInicial = (NumeroInicial +1);
+            }
+    }
+}
